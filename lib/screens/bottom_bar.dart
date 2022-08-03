0fc54b1f1ca4d6cac87cecefa2bfeed7d7b6d7d5
dimas <dimhko@gmail.com>
2022-08-03 00:16:11 -0400

    changes

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text(""),),
+      body: const Center(
+        child: Text(
+          "My Body"
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+        BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label: "Ticket"),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+      ],),
+    );
+    return Scaffold(
+      appBar: AppBar(title: const Text(""),),
+      body: const Center(
+        child: Text(
+          "My Body"
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+        BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label: "Ticket"),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+      ],),
+    );
+  }
+}
